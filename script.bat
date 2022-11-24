ff797a4814626a4dd1aab850fc42590dba5de2f6
Jelson Matheus <jelsonmatheus200f@gmail.com>
2022-11-24 08:43:51 -0300

    Adiciona script .bat

---
+++ script.bat
@@ -0,0 +1,6 @@
+1 - Rodar o sql banco_limpo.sql no banco
+2 - php artisan migrate
+3 - php artisan db:seed --class="GestorTableSeeder"
+4 - Subi os insert da tabela 'configuracoes' - captec_caxias_base.sql
+5 - Rodar o sql views_rupert.sql 
+
